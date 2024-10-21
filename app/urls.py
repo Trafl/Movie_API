2b6d420347574bcc38de5f1a872263e2cfe1187e
P.Ivo <pivolluz@gmail.com>
2024-10-21 20:53:08 -0300

    Feature: Adiciona Url e View para Listar os generos

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,8 @@
-from tkinter.font import names
-
 from django.contrib import admin
 from django.urls import path
+from genres.views import genre_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('genres/', genre_list_view, name= 'genre-list'),
+    path('genres/', genre_view, name='genre-list'),
 ]
