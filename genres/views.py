fa5f1b1d568c13af5e222bb37c7bf7682c4936c3
P.Ivo <pivolluz@gmail.com>
2024-10-21 22:46:51 -0300

    Feature: Adiciona View para atualizar um Genre

--- genres/views.py
+++ genres/views.py
@@ -4,6 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import get_object_or_404
 from genres.models import Genre
 
+
 @csrf_exempt
 def genre_create_list_view(request):
     if request.method == 'GET':
@@ -17,12 +18,18 @@ def genre_create_list_view(request):
         new_genre.save()
         return JsonResponse({'id': new_genre.id, 'name': new_genre.name}, status=201)
 
+
 @csrf_exempt
 def genre_detail_view(request, pk):
+    genre = get_object_or_404(Genre, pk=pk)
+
     if request.method == 'GET':
-        genre = get_object_or_404(Genre, pk=pk)
         data = {'id': genre.id, 'name': genre.name}
         return JsonResponse(data)
 
-
+    elif request.method == 'PUT':
+        data = json.loads(request.body.decode('utf8'))
+        genre.name = data['name']
+        genre.save()
+        return JsonResponse({'id': genre.id, 'name': genre.name})
 
